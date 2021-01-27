ecff16dd7acd1cafdcb9517a65e808361583f181
yilmazchef <yilmaz@mail.be>
2021-01-27 21:42:15 +0100

    ready to test on heroku ..

--- src/main/java/be/fooda/backend/address/FoodaAddressApp.java
+++ src/main/java/be/fooda/backend/address/FoodaAddressApp.java
@@ -1,15 +1,17 @@
 package be.fooda.backend.address;
 
+import be.fooda.backend.address.config.LuceneIndexConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @EnableEurekaClient
 public class FoodaAddressApp {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FoodaAddressApp.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FoodaAddressApp.class, args);
+    }
 
 }
