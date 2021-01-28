48ca48770557982b8a78b8523856704dcb78bcc4
yilmazchef <yilmaz@mail.be>
2021-01-28 15:13:56 +0100

    hibernate ddl is set to validate

--- src/main/java/be/fooda/backend/address/FoodaAddressApp.java
+++ src/main/java/be/fooda/backend/address/FoodaAddressApp.java
@@ -4,14 +4,12 @@ import be.fooda.backend.address.config.LuceneIndexConfig;
 import be.fooda.backend.address.config.RestClientConfig;
 import be.fooda.backend.address.config.SwaggerConfig;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@Configuration
-@EnableAutoConfiguration
+@SpringBootApplication
 @EnableEurekaClient
 @EnableSwagger2
 @Import({
