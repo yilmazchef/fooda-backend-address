59403fbfb6e9b91462e23b4e55b94e13753df898
yilmazchef <yilmaz@mail.be>
2021-01-28 14:32:47 +0100

    procfile is added.

--- src/main/java/be/fooda/backend/address/FoodaAddressApp.java
+++ src/main/java/be/fooda/backend/address/FoodaAddressApp.java
@@ -1,10 +1,8 @@
 package be.fooda.backend.address;
 
-import be.fooda.backend.address.config.LuceneIndexConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @EnableEurekaClient
