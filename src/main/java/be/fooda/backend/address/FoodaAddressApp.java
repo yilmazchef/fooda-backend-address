2415b122b1ea091885d4d95bb56de183afee3800
yilmazchef <yilmaz@mail.be>
2021-01-28 14:42:09 +0100

    fixes on legacy code.

--- src/main/java/be/fooda/backend/address/FoodaAddressApp.java
+++ src/main/java/be/fooda/backend/address/FoodaAddressApp.java
@@ -1,10 +1,12 @@
 package be.fooda.backend.address;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
 @EnableEurekaClient
 public class FoodaAddressApp {
 
