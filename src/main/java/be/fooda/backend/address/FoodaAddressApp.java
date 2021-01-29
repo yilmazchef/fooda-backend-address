b07215a1939f2bcc8cf91296248cbca0ab8b7544
yilmazchef <yilmaz@mail.be>
2021-01-29 22:57:18 +0100

    spring security fixes must be done. still being tested.

--- src/main/java/be/fooda/backend/address/FoodaAddressApp.java
+++ src/main/java/be/fooda/backend/address/FoodaAddressApp.java
@@ -3,6 +3,7 @@ package be.fooda.backend.address;
 import be.fooda.backend.address.config.LuceneIndexConfig;
 import be.fooda.backend.address.config.RestClientConfig;
 import be.fooda.backend.address.config.SwaggerConfig;
+import be.fooda.backend.address.config.WebSecurityConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -13,7 +14,10 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableEurekaClient
 @EnableSwagger2
 @Import({
-        LuceneIndexConfig.class, RestClientConfig.class, SwaggerConfig.class
+        LuceneIndexConfig.class,
+        RestClientConfig.class,
+        SwaggerConfig.class,
+        WebSecurityConfig.class
 })
 public class FoodaAddressApp {
 
