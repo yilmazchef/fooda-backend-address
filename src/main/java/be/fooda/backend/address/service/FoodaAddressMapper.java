dc0d61d6b29063da6485fdd70a824c3fb09e9116
yilmazchef <yilmaz@mail.be>
2021-01-30 09:38:51 +0100

    mapping annotation settings are corrected.

--- src/main/java/be/fooda/backend/address/service/FoodaAddressMapper.java
+++ src/main/java/be/fooda/backend/address/service/FoodaAddressMapper.java
@@ -5,31 +5,32 @@ import be.fooda.backend.address.model.entity.FoodaAddress;
 import be.fooda.backend.address.model.create.FoodaAddressCreate;
 import be.fooda.backend.address.model.read.FoodaAddressExample;
 import be.fooda.backend.address.model.update.FoodaAddressUpdate;
-import org.mapstruct.BeanMapping;
-import org.mapstruct.Mapper;
-import org.mapstruct.MappingTarget;
-import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.*;
 
-@Mapper(componentModel = "spring")
+import static org.mapstruct.CollectionMappingStrategy.ADDER_PREFERRED;
+import static org.mapstruct.NullValueCheckStrategy.ALWAYS;
+
+@Mapper(
+        componentModel = "spring",
+        nullValueCheckStrategy = ALWAYS,
+        collectionMappingStrategy = ADDER_PREFERRED
+)
 public interface FoodaAddressMapper {
 
-     FoodaAddress fromCreateToEntity( FoodaAddressCreate from);
+    FoodaAddress fromCreateToEntity(FoodaAddressCreate from);
 
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
-     FoodaAddress fromUpdateToEntity(FoodaAddressUpdate from, @MappingTarget  FoodaAddress to);
+    FoodaAddress fromUpdateToEntity(FoodaAddressUpdate from, @MappingTarget FoodaAddress to);
 
+    FoodaAddress fromExampleToEntity(FoodaAddressExample from);
 
-
-     FoodaAddress fromExampleToEntity( FoodaAddressExample from);
-
-     FoodaAddressCreate fromEntityToCreate( FoodaAddress from);
+    FoodaAddressCreate fromEntityToCreate(FoodaAddress from);
 
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
-     FoodaAddressUpdate fromEntityToUpdate( FoodaAddress from, @MappingTarget  FoodaAddressUpdate to);
+    FoodaAddressUpdate fromEntityToUpdate(FoodaAddress from, @MappingTarget FoodaAddressUpdate to);
 
+    FoodaAddressExample fromEntityToExample(FoodaAddress from);
 
-     FoodaAddressExample fromEntityToExample( FoodaAddress from);
-
-    FoodaAddressExample fromEntityToResponse( FoodaAddress from);
+    FoodaAddressExample fromEntityToResponse(FoodaAddress from);
 
 }
